a5d2a8c21d8ca35c085fb7bcb68bc0d7948c4758
Trinath Kella <148771994+trinathkella@users.noreply.github.com>
2025-05-02 14:04:39 +0530

    Create bin_to_one_hot.sv

---
+++ Day8/bin_to_one_hot.sv
@@ -0,0 +1,14 @@
+// Binary to one-hot
+
+module day8#(
+  parameter BIN_W       = 4,
+  parameter ONE_HOT_W   = 16
+)(
+  input   wire[BIN_W-1:0]     bin_i,
+  output  wire[ONE_HOT_W-1:0] one_hot_o
+);
+
+  // Write your logic here...
+ assign one_hot_o = 1'b1 << bin_i;
+
+endmodule
