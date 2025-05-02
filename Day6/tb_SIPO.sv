3bc97888dc5e69802204d9ab8d0dd64b76b68a5e
Trinath Kella <148771994+trinathkella@users.noreply.github.com>
2025-05-02 10:16:55 +0530

    Update tb_SIPO.sv

--- Day6/tb_SIPO.sv
+++ Day6/tb_SIPO.sv
@@ -1,1 +1,31 @@
+module day6_tb ();
 
+  // Write your Testbench here...
+    reg 			clk;
+    reg 			reset;
+    reg 			x_i;
+  	wire [3:0]sr_o;
+  	
+  	day6 d(.*);
+  	always begin
+      	clk = 1'b1;
+      #5;
+      	clk = 1'b0;
+      #5;
+    end
+  
+  	initial begin
+    		reset = 1'b1;
+      	x_i   = 1'b1;
+      	@(posedge clk);
+      	reset = 1'b0;
+      	@(posedge clk);
+      for(int i = 0; i < 16; i++)
+        begin
+        		x_i = $random % 2;
+          	@(posedge clk);
+        end
+      $finish();
+    end
+  
+endmodule
