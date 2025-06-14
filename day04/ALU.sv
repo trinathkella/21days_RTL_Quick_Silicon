a6b92d2453b0c632cc4b742b87d6eb62f37cc513
Trinath Kella <148771994+trinathkella@users.noreply.github.com>
2025-06-14 20:31:32 +0530

    Update ALU.sv

--- day04/ALU.sv
+++ day04/ALU.sv
@@ -1,25 +1,35 @@
 // A simple ALU
 
-module day4 (
-  input     logic [7:0]   a_i,
-  input     logic [7:0]   b_i,
-  input     logic [2:0]   op_i,
+module ALU
+(
+	input     logic [7:0]   a_i,
+	input     logic [7:0]   b_i,
+	input     logic [2:0]   op_i,
+	output    logic [7:0]   alu_o
+);	
+	
+	logic carry;
 
-  output    logic [7:0]   alu_o
-);
+	localparam ADD	= 3'b000;
+	localparam SUB	= 3'b001;
+	localparam SLL	= 3'b010;
+	localparam LSR	= 3'b011;
+	localparam AND	= 3'b100;
+	localparam OR	= 3'b101;
+	localparam XOR	= 3'b110;
+	localparam EQL	= 3'b111;
 
-  // Write your logic here...
-  always @ (*) begin
-    case(op_i)
-      	3'b000 : alu_o = a_i + b_i;
-      	3'b001 : alu_o = a_i - b_i;
-      	3'b010 : alu_o = a_i << b_i;
-        3'b011 : alu_o = a_i >> b_i;
-      	3'b100 : alu_o = a_i & b_i;
-      	3'b101 : alu_o = a_i | b_i;
-      	3'b110 : alu_o = a_i ^ b_i;
-      	3'b111 : alu_o = {7'b0, (a_i == b_i)};
-    endcase
-  end
+	always_comb begin
+		case(op_i)
+			ADD : {carry, alu_o} = a_i +  b_i;
+			SUB : 		   alu_o = a_i -  b_i;
+			SLL : 		   alu_o = a_i << b_i[2:0];
+			LSR :  		   alu_o = a_i >> b_i[2:0];
+			AND :		   alu_o = a_i &  b_i;
+			OR  : 		   alu_o = a_i |  b_i;
+			XOR : 		   alu_o = a_i ^  b_i;
+			EQL :		   alu_o = (a_i == b_i) ? 8'd1 : 8'd0;
+		endcase
+	end
 
-endmodule
+endmodule : ALU
